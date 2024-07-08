07c9707694e5f514ca12c1ab6317da95f10633b1
Tapiwa <tmotsi10@gmail.com>
2024-07-08 14:03:34 +0200

    first commit

---
+++ lesson01/build/js/main.js
@@ -0,0 +1,12 @@
+"use strict";
+let username = 'Macdonald';
+console.log(username);
+let a = 12;
+let b = '6';
+let c = 2;
+console.log(a / b); // its a valid JS but invalid Ts
+console.log(c * b); // its a valid JS but invalid Ts
+//run tsc main.ts -w for live changes
+//run tsc --init
+//"outDir": "./build/js",  
+// "rootDir": "./src",   
---
+++ lesson01/src/main.ts
@@ -0,0 +1,14 @@
+let username = 'Macdonald'
+console.log(username)
+
+let a: number  = 12
+let b: string = '6'
+let c: number = 2
+
+console.log(a / b) // its a valid JS but invalid Ts
+
+console.log(c * b) // its a valid JS but invalid Ts
+//run tsc main.ts -w for live changes
+//run tsc --init
+//"outDir": "./build/js",  
+// "rootDir": "./src",   
